27619ad03735b75dddab49eaa5c52ac3f819c260
Fraidoon Pourooshasb <fpourooshasb@gmail.com>
2023-10-30 11:44:43 -0400

    placing leak outside square for bot 1

--- Ship.py
+++ Ship.py
@@ -15,6 +15,7 @@ class Ship():
         # Initialize bot and leak positions
         self.bot = (-1, -1)
         self.leak = (-1, -1)
+        self.k_val = 0
 
     def __repr__(self):
         ship_str = ""
@@ -49,58 +50,65 @@ class Ship():
                 self.ship[rand_x_coord][rand_y_coord] = self.colored_block('c')
                 self.bot = (rand_x_coord, rand_y_coord)
             elif self.leak == (-1, -1):
-                self.ship[rand_x_coord][rand_y_coord] = self.colored_block('g')
-                self.leak = (rand_x_coord, rand_y_coord)
+                # Ensure the leak is at least k_val + 1 cells away from the square
+                while True:
+                    leak_x = random.randint(0, self.D - 1)
+                    leak_y = random.randint(0, self.D - 1)
+                    if (abs(leak_x - self.bot[0]) > self.k_val + 1 or abs(leak_y - self.bot[1]) > self.k_val + 1):
+                        self.ship[leak_x][leak_y] = self.colored_block('g')
+                        self.leak = (leak_x, leak_y)
+                        break
 
-    def run_bot_1(self, probability):
+    def run_bot_1(self, k_val):
         pass
 
-    def run_bot_2(self, probability):
+    def run_bot_2(self, k_valability):
         pass
 
-    def run_bot_3(self, probability):
+    def run_bot_3(self, k_valability):
         pass
 
-    def run_bot_5(self, probability):
+    def run_bot_5(self, k_valability):
         pass
     
-    def run_bot_6(self, probability):
+    def run_bot_6(self, k_valability):
         pass
     
-    def run_bot_7(self, probability):
+    def run_bot_7(self, k_valability):
         pass
     
-    def run_bot_8(self, probability):
+    def run_bot_8(self, k_valability):
         pass
     
-    def run_bot_9(self, probability):
+    def run_bot_9(self, k_valability):
         pass
 
 if __name__ == "__main__":
     ship = Ship()
+    k_val= float(input("Enter k value: "))
+    ship.k_val = k_val
     ship.generate_ship()
     print(ship)
 
     ans = int(input("Which bot do you want to run?\n1.Bot 1\n2.Bot 2\n3.Bot 3\n4.Bot 4\nBot: "))
-    prob = float(input("What is the probability: "))
 
     if ans == 1:
-        ship.run_bot_1(prob)
+        ship.run_bot_1(k_val)
     elif ans == 2:
-        ship.run_bot_2(prob)
+        ship.run_bot_2(k_val)
     elif ans == 3:
-        ship.run_bot_3(prob)
+        ship.run_bot_3(k_val)
     elif ans == 4:
-        ship.run_bot_4(prob)
+        ship.run_bot_4(k_val)
     elif ans == 5:
-        ship.run_bot_5(prob)
+        ship.run_bot_5(k_val)
     elif ans == 6:
-        ship.run_bot_6(prob)
+        ship.run_bot_6(k_val)
     elif ans == 7:
-        ship.run_bot_7(prob)
+        ship.run_bot_7(k_val)
     elif ans == 8:
-        ship.run_bot_8(prob)
+        ship.run_bot_8(k_val)
     elif ans == 9:
-        ship.run_bot_9(prob)
+        ship.run_bot_9(k_val)
     else:
         print("Invalid bot choice.")
